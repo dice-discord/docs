0f89bfd77e0abeb12edd0d6b24fcd1d69e662c37
Jonah Snider <jonah@jonah.pw>
2020-11-05 12:01:52 -0800

    style: format with Prettier

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,5 @@
 module.exports = {
   globDirectory: "site/",
-  globPatterns: [
-    "**/*.{html,png,js,css,json,xml}",
-  ],
+  globPatterns: ["**/*.{html,png,js,css,json,xml}"],
   swDest: "site/sw.js",
 };
